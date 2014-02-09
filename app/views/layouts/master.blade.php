2125a5d8b654deea546b9a423a1f9f075d980d70
Bodin Samuel <bodin.samuel@gmail.com>
2014-02-09 19:11:32 +0000

    Change routes to fit initial need (home) +default file

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <title>Eazy'Home</title>
+    {{ HTML::style('css/style.css') }}
+</head>
+<body>
+    <header id="main">
+        <nav>
+            
+        </nav>
+    </header>
+    <div id="container">
+        <div id="content">
+            @yield('content')
+        </div>
+    </div>
+    <footer>
+        
+    </footer>
+</body>
+</html>
