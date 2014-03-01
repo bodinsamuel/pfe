1c941ba15df6ad7aea3ede36e3f409c4f95adb18
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-01 01:22:50 +0000

    Layouts > Master: update layout to be more flexible

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
-    <title>Eazy'Home</title>
-    {{ HTML::style('css/style.css') }}
+    <title>
+        @if (isset($__page_title)) {{{ $__page_title }}} | @endif
+        {{{ $__meta_title }}}
+    </title>
+    {{ HTML::style('assets/css/layouts/master.css') }}
 </head>
-<body>
-    <header id="main">
+<body id="__{{{ $__current_controller }}}_{{{ $__current_method }}}"
+      class="_c_{{{ $__current_controller }}} _m_{{{ $__current_method }}}">
+    <header id="header">
         <nav>
-            
+            <a href="/">Home</a>
         </nav>
     </header>
     <div id="container">
@@ -17,8 +21,10 @@
             @yield('content')
         </div>
     </div>
-    <footer>
-        
+    <footer id="footer">
+        <div class="inner">
+            © 2014 -
+        </div>
     </footer>
 </body>
 </html>
