2c720becd680c363946ce1377397e00bee662eba
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-01 01:21:39 +0000

    Account > Register

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -8,7 +8,53 @@ class AccountController extends BaseController
         return View::make('account/dashboard');
     }
 
-    public function register()
+    public function get_Register()
+    {
+        $data = ['__page_title' => 'Register'];
+        return View::make('account/register', $data);
+    }
+
+    public function post_Register()
+    {
+        $data = ['__page_title' => 'Register'];
+
+        $validator = User::validateRegister();
+        if (!$validator->fails())
+        {
+            $user = new User;
+            $user->email = Input::get('email');
+            $user->status = 0;
+            $user->password = Hash::make(Input::get('password'));
+            $inserted = $user->save();
+
+            // Insertion succesfull
+            if ($inserted === TRUE)
+            {
+                return Redirect::to('/')
+                        ->with('flash_notice', 'You have succesfully register, '
+                                                . 'please validate your account by clicking '
+                                                . 'the link on the email you just recieved.');
+            }
+            else
+            {
+
+            }
+        }
+
+        return Redirect::to('register')->withInput()->withErrors($validator);
+    }
+
+    public function get_Login()
+    {
+        # code...
+    }
+
+    public function post_Login()
+    {
+        # code...
+    }
+
+    public function get_Logout()
     {
         # code...
     }
---
+++ app/views/account/register.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('content')
+    {{ Form::open(array('action' => 'AccountController@get_Register', 'method' => 'post')) }}
+
+        <fieldset class="{{{ $errors->has('email') ? '_error' : '' }}}">
+            {{ Form::label('email', 'E-Mail') }}
+            {{ Form::text('email', Input::old('email'), array('placeholder' => 'john.doe@example.com')) }}
+            {{ $errors->first('email', '<span class="_msg _error">:message</span>') }}
+        </fieldset>
+
+        <fieldset class="{{{ $errors->has('email') ? '_error' : '' }}}">
+            {{ Form::label('password') }}
+            {{ Form::password('password', '', array('placeholder' => 'password')) }}
+            {{ $errors->first('password', '<span class="_msg _error">:message</span>') }}
+        </fieldset>
+
+        <div class="action">
+            {{ Form::submit('Register') }}
+        </div>
+    {{ Form::close() }}
+@stop
