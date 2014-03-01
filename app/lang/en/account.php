d17a84a6ee0be7e1d0cac82771dd89c406b8bca5
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-01 14:35:08 +0000

    Language file for account

---
+++ app/lang/en/account.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'success' => [
+        'register' => 'You have succesfully register, please validate your account by clicking the link on the email you just recieved.',
+
+        'login' => '',
+
+        'logout' => 'You succesfully logout, see you soon.',
+    ],
+
+    'error' => [
+        'login' => 'Your username/password combination was incorrect.'
+    ],
+];
