129d78b12bf17243b056f67cc3430afaed1c65af
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-02 19:02:35 +0000

    Lang++

--- app/lang/en/account.php
+++ app/lang/en/account.php
@@ -3,16 +3,20 @@
 return [
 
     'success' => [
-        'register' => 'You have succesfully register, please validate your account by clicking the link on the email you just recieved.',
+        'register' => 'You have succesfully register, please validate your account by clicking the link on the email you just recieved',
 
         'login' => '',
 
-        'logout' => 'You succesfully logout, see you soon.',
+        'logout' => 'You succesfully logout, see you soon',
 
-        'send_forgot_password' => 'An email has been sent to you to get your new password.'
+        'password' => [
+            'send_forgot' => 'An email has been sent to you to get your new password',
+            'reseted' => 'Your password has been reseted correctly'
+        ]
     ],
 
     'error' => [
-        'login' => 'Your username/password combination was incorrect.'
+        'email_not_verified' => 'Please validate your email before login',
+        'login' => 'Your username/password combination was incorrect'
     ],
 ];
