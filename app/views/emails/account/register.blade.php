6fc3e1955846ccff4e68a968e69ccefcf7ed1a38
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-16 10:27:22 +0000

    Emails > Account: rename folder +validation +register

---
+++ app/views/emails/account/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Welcome</h2>
+
+        <div>
+            To activate your account please clik on this link
+            {{ URL::route('account_validate', ['token' => $token, 'email' => $user['email']]) }}.
+        </div>
+        <div id="disclaimer">
+
+        </div>
+    </body>
+</html>
