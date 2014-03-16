f616bfd8a68441bd8432201953b814b77a98225b
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-16 10:29:38 +0000

    Lang > Account: add more sentences

--- app/lang/en/account.php
+++ app/lang/en/account.php
@@ -5,18 +5,23 @@ return [
     'success' => [
         'register' => 'You have succesfully register, please validate your account by clicking the link on the email you just recieved',
 
-        'login' => '',
+        'login' => 'Welcome back !',
 
         'logout' => 'You succesfully logout, see you soon',
 
         'password' => [
             'send_forgot' => 'An email has been sent to you to get your new password',
             'reseted' => 'Your password has been reseted correctly'
+        ],
+
+        'validation' => [
+            'sent' => 'An email has been sent to you with the link.',
+            'done' => 'You have succesfully validated your account. Enjoy :)'
         ]
     ],
 
     'error' => [
-        'email_not_verified' => 'Please validate your email before login',
+        'email_not_verified' => 'Please validate your email before login. <br> Never recieved the mail? <a href="/account/send_validation?email=:mail">Send me another !</a>',
         'login' => 'Your username/password combination was incorrect'
     ],
 ];
