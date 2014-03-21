0d5acc695878438a245dd9db6973259fb4652841
Bodin Samuel <bodin.samuel@gmail.com>
2014-03-21 21:30:06 +0000

    Home: add config for map

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,9 +6,12 @@ class HomeController extends BaseController
     /**
      * Homepage
      */
-	public function run()
-	{
-		return View::make('home/home');
-	}
+    public function run()
+    {
+        $data = ['__with_bg_map' => TRUE,
+                 'map_config' => ['locate' => TRUE]];
+
+        return View::make('home/home', $data);
+    }
 
 }
