acf24a63f0de090dd0631fe2137033bbf2af63c1
Bodin Samuel <bodin.samuel@gmail.com>
2014-05-30 17:25:07 +0000

    Media: fix uploader + add width, height, extension, select

--- app/libraries/Custom/Media.php
+++ app/libraries/Custom/Media.php
@@ -7,25 +7,32 @@ class Media
     const TYPE_AUDIO = 3;
     const TYPE_OTHER = 4;
 
+    const URL_ACCESS = 'http://media.pfe.dev';
+    const UPLOAD_DIR = '/var/www/pfe.dev/media';
+
     public static function create($inputs)
     {
-        $inputs = array_only($inputs, [
-            'id_gallery', 'type', 'mime', 'hash'
-        ]);
+        $inputs = array_fill_base([
+            'id_gallery', 'type', 'extension', 'mime', 'hash', 'title',
+            'width', 'height'
+        ], $inputs);
         $inputs['status'] = Cnst::NEED_VALIDATION;
         $inputs['id_user'] = \User::getIdOrZero();
+        $inputs['title'] = \Str::slug($inputs['title']);
 
         $query = 'INSERT INTO media
-                              (`id_gallery`, `id_user`, `type`, `mime`, `hash`,
-                               `status`, `date_created`, `date_updated`)
-                       VALUES (:id_gallery, :id_user, :type, :mime, :hash,
-                               :status, NOW(), NOW())';
+                              (`id_gallery`, `id_user`, `type`, `extension`, `mime`,
+                               `hash`, `width`, `height`, `title`, `status`,
+                               `date_created`, `date_updated`)
+                       VALUES (:id_gallery, :id_user, :type, :extension, :mime,
+                               :hash, :width, :height, :title, :status,
+                               NOW(), NOW())';
 
         $stmt = \DB::statement($query, $inputs);
         if ($stmt === FALSE)
             return -1;
 
-        $update_gallery = Gallery::update_media_count();
+        $update_gallery = Gallery::auto_update($inputs['id_gallery']);
 
         return \DB::getPdo()->lastInsertId();
     }
@@ -35,11 +42,33 @@ class Media
         return \Validator::make(
             $inputs, [
                 'id_gallery' => 'required|integer',
-                'type' => 'required|integer',
-                'mime' => 'required|string',
-                'hash' => 'required|string',
+                'type'       => 'required|integer',
+                'extension'  => 'required|string',
+                'mime'       => 'required|string',
+                'hash'       => 'required|string',
+                'title'      => 'required|string',
+                'width'       => 'required|integer',
+                'height'      => 'required|integer'
             ]
         );
+    }
+
+    public static function select($id_media)
+    {
+        $query = 'SELECT id_media,
+                         id_user,
+                         type,
+                         extension,
+                         mime,
+                         hash,
+                         title,
+                         status,
+                         date_created,
+                         date_updated
+                    FROM media
+                   WHERE id_media = :id_media';
+
+        return \DB::select($query, ['id_media' => $id_media]);
     }
 
     public static function upload($id_gallery, array $medias)
@@ -48,7 +77,7 @@ class Media
 
         $uploader = new Media\Uploader();
         $uploader->setAllowed(['image/jpeg', 'image/png']);
-        $uploader->setDir('/var/www/pfe.dev/media');
+        $uploader->setDir(self::UPLOAD_DIR);
         $uploader->setMaxFileSize(1024 * 1024 * 10);
 
         foreach ($medias as $media)
@@ -70,9 +99,13 @@ class Media
                 // Creation media in DB
                 $creation = Media::create([
                     'id_gallery' => $id_gallery,
+                    'title' => $media['title'],
                     'type' => $up['infos']['type'],
                     'mime' => $up['infos']['mime'],
-                    'hash' => $up['hash']
+                    'width' => $up['infos']['width'],
+                    'height' => $up['infos']['height'],
+                    'hash' => $up['hash'],
+                    'extension' => $up['infos']['ext_safe']
                 ]);
 
                 // Check Error
@@ -89,5 +122,15 @@ class Media
         }
 
         return $return;
+    }
+
+    public static function url($m, $ratio)
+    {
+        if (empty($m))
+            return self::URL_ACCESS . '/404-'. $ratio . '-0-not-found.jpg';
+
+        $m = (array)$m;
+        return self::URL_ACCESS . '/' . $m['hash'] . '-' . $ratio . '-' .
+               $m['id_media'] . '-' . $m['title'] . '.' . $m['extension'];
     }
 }
