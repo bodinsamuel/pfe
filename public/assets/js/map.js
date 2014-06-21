e181ac9e1278afd1af8b18aaa3c93fadf14023c1
Bodin Samuel <bodin.samuel@gmail.com>
2014-06-21 20:39:31 +0000

    Map: css + js + icon

---
+++ public/assets/js/map.js
@@ -0,0 +1,53 @@
+Map = (function() {
+
+    function Map()
+    {
+        this.map = {};
+        this.featureLayer = {};
+    }
+
+
+    Map.prototype.init = function()
+    {
+        this.map = L.mapbox.map('mapbox-screen-map', 'bodinsamuel.hj2ocb3b')
+                        .setView([48.855, 2.32], 12);
+
+        this.featureLayer = L.mapbox.featureLayer().addTo(this.map);
+
+        L.control.locate().addTo(this.map).setPosition('topright');
+        this.map.zoomControl.setPosition('topright');
+
+        this.icons = Map.prototype.customIcon();
+    };
+
+    Map.prototype.parseMarkers = function(json)
+    {
+        var markers = new L.MarkerClusterGroup();
+
+        for (var i in json)
+        {
+            marker = json[i];
+            var marker = L.marker(new L.LatLng(json[i].y, json[i].x), {
+                icon: this.icons.test1,
+                title: json[i].title
+            });
+            marker.bindPopup(json[i].title);
+            markers.addLayer(marker);
+        };
+        this.map.addLayer(markers);
+    };
+
+    Map.prototype.customIcon = function()
+    {
+        return {
+            test1: L.icon({
+                "iconUrl": "/assets/img/map/pin.png",
+                "iconSize": [25, 25], // size of the icon
+                "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+                "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+            })
+        };
+    };
+
+    return Map;
+})();
