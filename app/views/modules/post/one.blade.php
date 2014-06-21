c3377ffb6f77cce32c3999104fd79331cce5ceed
Bodin Samuel <bodin.samuel@gmail.com>
2014-06-21 12:14:26 +0000

    tpl for post

---
+++ app/views/modules/post/one.blade.php
@@ -0,0 +1,8 @@
+<div class="galerie mini">
+    <div class="big">
+        <img src="{{{ $post['cover']['url'] }}}" width="200" alt="" class="" />
+    </div>
+</div>
+<div class="content">
+    {{{ $post['content'] }}}
+</div>
