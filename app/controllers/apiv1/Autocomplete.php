e7683710f4540ef89bd26b9739d90233366f4f66
Bodin Samuel <bodin.samuel@gmail.com>
2014-06-22 15:08:39 +0000

    Api > autocomplete: split location in 3 datasets, for typeahead

--- app/controllers/apiv1/Autocomplete.php
+++ app/controllers/apiv1/Autocomplete.php
@@ -20,13 +20,63 @@ class Autocomplete extends \BaseController
         $states = \Custom\Geo::search_states($q);
 
         return \Response::json([
-            'time' => time(),
             'error' => FALSE,
             'data' => [
                 [ 'name' => 'States', 'children' => $states ],
                 [ 'name' => 'Provinces', 'children' => $provinces ],
                 [ 'name' => 'Cities', 'children' => $cities ]
             ]
+        ], 200);
+    }
+
+    public function getStates()
+    {
+        $q = \Input::get('q');
+        if (!$q)
+        {
+            return \Response::json([
+                'error' => 'bad_request',
+                'data' => []
+            ], 400);
+        }
+
+        return \Response::json([
+            'error' => FALSE,
+            'data' => \Custom\Geo::search_states($q)
+        ], 200);
+    }
+
+    public function getProvinces()
+    {
+        $q = \Input::get('q');
+        if (!$q)
+        {
+            return \Response::json([
+                'error' => 'bad_request',
+                'data' => []
+            ], 400);
+        }
+
+        return \Response::json([
+            'error' => FALSE,
+            'data' => \Custom\Geo::search_provinces($q)
+        ], 200);
+    }
+
+    public function getCities()
+    {
+        $q = \Input::get('q');
+        if (!$q)
+        {
+            return \Response::json([
+                'error' => 'bad_request',
+                'data' => []
+            ], 400);
+        }
+
+        return \Response::json([
+            'error' => FALSE,
+            'data' => \Custom\Geo::search_cities($q)
         ], 200);
     }
 }
