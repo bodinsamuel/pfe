93c0cd57bbcffc960ec76f977d570409c0caadf2
Bodin Samuel <bodin.samuel@gmail.com>
2014-06-26 20:02:22 +0000

    config: fix alpha

--- app/config/alpha/app.php
+++ app/config/alpha/app.php
@@ -7,8 +7,8 @@ return array(
 	'url' => 'http://alpha.loge-ici.fr',
     'domain' => [
         'main' => 'alpha.loge-ici.fr',
-        'admin' => 'http://admin.alpha.loge-ici.fr',
-        'media' => 'http://media.alpha.loge-ici.fr'
+        'admin' => 'admin.alpha.loge-ici.fr',
+        'media' => 'media.alpha.loge-ici.fr'
     ],
     'media_dir' => '/home/media.loge-ici',
 );
