cf06dc4a87dbc9db0b0e5f3a5be6b0715ad7e356
bodinsamuel <bodin.samuel@gmail.com>
2014-06-27 03:39:38 +0200

    Config mail alpha

--- app/config/alpha/mail.php
+++ app/config/alpha/mail.php
@@ -1,5 +1,9 @@
 <?php
 
 return array(
+    'driver' => 'mail',
     'host' => 'smtp.loge-ici.fr',
+    'encryption' => false,
+    'port' => 25,
+    'from' => ['address' => 'postmaster@loge-ici.fr', 'name' => 'Dev'],
 );
